5903c959d74382fac224346fb595cb6a9dad8b53
maxai <maxai@MAX>
2023-04-16 11:05:06 +0800

    Add project files.

---
+++ sumOfTwoElementsInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] nums = { 1, 2, 3, 4, 5, 6 };
+int target = 10;
+
+for (int j = nums.Length - 1; 0 <= j - 1; j--)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i != j && nums[i] + nums[j] == target)
+        {
+
+            Console.WriteLine((i, j));
+        }
+
+    }
+}
